94b1fb9ad1565ae1d9e2c2590582f002294d4c5a
Alisson <aamachado@inf.ufsm.br>
2022-04-13 17:11:58 -0300

    add some tailwind custom classes * colors * sizes directives * bg 'white-blue' in all page

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
     './js/**/*.js',
     '../lib/*_web/**/*.*ex'
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'white-blue': '#EBF4FF',
+        'white-green': '#F5FBEE',
+        'white-purple': '#FFEBFA',
+        'white-red': '#FFEBEB',
+        'opal': '#a5cabf',
+        'coral': '#E88787'
+      },
+      maxWidth: {
+          '1/4': '25%',
+          '1/2': '50%',
+          '3/4': '75%',
+      },
+      maxHeight: {
+          '1/4': '25%',
+          '1/2': '50%',
+          '3/4': '75%'
+      },
+    }
+  },
   variants: {},
   plugins: []
-};
+}
